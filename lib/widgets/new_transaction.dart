fe95386e15a1100e9ac594a83b2394ca4ca87713
Omar Gamal AbdelMageed <56756748+fiend361@users.noreply.github.com>
2021-01-05 23:53:58 +0200

    updated UI

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -49,9 +49,29 @@ class _NewTransactionState extends State<NewTransaction> {
               keyboardType: TextInputType.number,
               onSubmitted: (_) => submitData(),
             ),
-            FlatButton(
-              child: Text('Add Transaction'),
-              textColor: Colors.red,
+            Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  Text('No Date Chosen!'),
+                  FlatButton(
+                    textColor: Colors.blue,
+                    onPressed: () {},
+                    child: Text(
+                      'Choose Date',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            RaisedButton(
+              child: Text(
+                'Add Transaction',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              color: Colors.blue,
+              textColor: Colors.white,
               onPressed: submitData,
             )
           ],
