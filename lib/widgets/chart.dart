769cf104efb5756a166066b871a2b1732dacb76e
Omar Gamal AbdelMageed <56756748+fiend361@users.noreply.github.com>
2021-01-27 21:59:59 +0200

    wired up chart widget

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -20,7 +20,10 @@ class Chart extends StatelessWidget {
         }
       }
 
-      return {'day': DateFormat.E(weekday), 'amount': totalSum};
+      return {
+        'day': DateFormat.E().format(weekday).substring(0, 1),
+        'amount': totalSum,
+      };
     });
   }
 
@@ -30,7 +33,9 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: <Widget>[],
+        children: groupegTransactionsValue.map((data) {
+          return Text('${data['day']}: ${data['amount']}');
+        }).toList(),
       ),
     );
   }
